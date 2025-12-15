0fda3a9675b146ea43863feb91b037d386ee2d4b
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-15 14:16:45 +0530

    Added a PHP info file for server configuration details.

---
+++ helloworld.php
@@ -0,0 +1,21 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello world</title>
+</head>
+<body>
+   
+<?php
+// single line comments are like
+# 
+/* our double line comment are here
+
+*/
+echo "Hello World!"; ?>  <br/>  
+<?php print "Hello World!"; ?>  <br/>
+<?php echo "Hello" . "World!"; ?>  <br/>
+<?php echo 2+3 ; ?>
+</body>
+</html>
