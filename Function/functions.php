4274d4a8bc4623f8bbb74998de0600be3615c210
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-15 19:24:19 +0530

    Add new functions for addition and subtraction, enhancing functionality with return values and conditional checks.

--- Function/functions.php
+++ Function/functions.php
@@ -25,15 +25,35 @@
 
         function say_hello3($gretting,$target,$punctuation){
             echo $gretting . "," . $target . ",". $punctuation . "<br />";
+            return true;
         }
         say_hello3("hello",$name,"!");
    ?>
 <br />
+
 <?php
     function addition($val1,$val2){
         $sum = $val1 + $val2;
-        echo addition(3,4);
+        return $sum; 
     }
+     $new_val = addition(3,4);
+     echo $new_val;
+
+     if(addition(5,6)==11){
+    echo "yes";    
+    }
+    function add_sub($val1,$val2){
+        $add = $val1 + $val2;
+        $sub = $val1 - $val2;
+        $result = array($add,$sub);
+        return $result; 
+    }
+    $result_array = add_sub(10,5);
+    echo "Add:".$result_array[0];
+    echo "SUB:".$result_array[1];
 ?>
+
+
+
 </body>
 </html>
