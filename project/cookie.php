09f45e66f59670016c8f6b278f633c8f974efcaa
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-15 22:11:28 +0530

    Enhance firstpage.php by refining dynamic link text generation for improved user engagement and interaction.

---
+++ project/cookie.php
@@ -0,0 +1,13 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookie</title>
+</head>
+<body>
+  <?php
+    setcookie('test',45,time()+(60*60*24*7));
+  ?>
+</body>
+</html>
---
+++ project/cookieread.php
@@ -0,0 +1,25 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reading Cookies</title>
+</head>
+<body>
+
+  <?php
+  $var1 = 0;
+  if(isset($_COOKIE["test"])){
+
+      $var1= $_COOKIE["test"];
+  }
+   echo $var1;
+  ?>
+
+  <?php
+  // this will delete cookie
+      setcookie('test',0,time()-(60*60*24*7));
+  ?>
+  
+</body>
+</html>
