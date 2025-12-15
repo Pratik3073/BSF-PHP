1dfb16e7518701c6c4e3a20f8a83bc8ed3b0dcc5
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-15 14:49:04 +0530

    Refactor srtings.php to enhance data type definitions

---
+++ Data-types/strings.php
@@ -0,0 +1,22 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings</title>
+</head>
+<body>
+   <?php
+        echo "hello world <br />";
+        echo 'hello world ';
+        $my_Variable = "hello world";
+        echo $my_Variable;
+        echo $my_Variable . "again" // concatenate 
+   ?><br />
+
+   <?php 
+        echo "{$my_Variable} again <br />";
+   ?>
+   
+</body>
+</html>
