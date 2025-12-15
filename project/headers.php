04a3c7d79bd102966f8d7ebbac1942bf160c0874
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-15 22:46:35 +0530

    Refine dynamic link text generation in firstpage.php to further enhance user engagement and interaction.

---
+++ project/headers.php
@@ -0,0 +1,22 @@
+<?php
+//this is how you return a 404 error
+
+//header("HTTP/1.0 404 Not Found");
+// ?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Headers</title>
+</head>
+
+<body>
+    <?php
+    //this is how you redirect a page
+    header("location: basic.html");
+    ?>
+
+</body>
+
+</html>
