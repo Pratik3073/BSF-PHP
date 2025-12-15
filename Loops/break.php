67e925ee9ed2149304888a8f570ed35a82cdd0e7
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-15 18:20:31 +0530

    Add PHP for-loop example using break statement

---
+++ Loops/break.php
@@ -0,0 +1,20 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Break</title>
+</head>
+<body>
+<?php
+        for($count = 0;$count <=10 ;$count++){
+            echo $count;
+            if($count == 10 ){
+                break;
+            }
+            echo ",";
+            
+        }
+   ?>
+</body>
+</html>
