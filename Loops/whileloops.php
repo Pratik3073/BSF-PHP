a594e0d8dd01ac5410b21a5e3e954adb770fcba1
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-15 17:45:09 +0530

    Refactor whileloops.php to improve code structure and enhance readability.

---
+++ Loops/whileloops.php
@@ -0,0 +1,32 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loops: While</title>
+</head>
+<body>
+   <?php 
+        $count = 0;
+        while($count <= 10){
+                echo $count . ",";
+                $count++;
+         }
+   echo "<br />Count:{$count}"
+   ?>
+<br />
+    <?php 
+        $count = 0;
+        while($count <= 10){
+            if($count ==5){
+                echo "FIVE";
+            }else {
+                echo $count . ",";
+            }
+                $count++;
+         }
+   echo "<br />Count:{$count}"
+   ?>
+
+</body>
+</html>
