1bf3b35ee8fb22fdca91a06c4c40c754afc68cca
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-15 17:25:26 +0530

    add logical operators, and implement type checking and casting.

--- Control_structures/logical.php
+++ Control_structures/logical.php
@@ -7,10 +7,10 @@
 </head>
 <body>
    <?php 
-        $a = 4;
+        $a = 5;
         $b = 4;
         if($a > $b){
-            echo "a is larger tha b ";
+            echo "a is larger than b ";
         } elseif($a == $b){
             echo " a equals b ";
         }
@@ -18,7 +18,37 @@
         else {
             echo "a is smaller than b ";
         }
-
    ?>
+
+   <br />
+    <?php 
+        $c = 1;
+        $d = 20;
+        if(($a > $b) && ($c > $d)){ //AND
+        echo "a is larger than b AND";
+        echo " c is larger than d";
+        }
+        if(($a > $b) || ($c > $d)){  //OR
+            echo "a is larger than b OR";
+            echo " c is larger than d";
+        } else{
+            echo "neither a is larger than b or c is larger than d ";
+        }
+    ?>
+    <br />
+    <?php
+        if(!isset($a)){
+            $a =100;;
+        }
+        echo $a;
+    ?>
+    <br />
+    <?php 
+    if(is_int($a)){
+        settype($a,"string");
+    }
+    echo gettype($a);
+    ?>
+
 </body>
 </html>
